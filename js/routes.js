90aecd3f475c461cba927b19d3911dc58e2789cd
yanpeng1028 <yp80112@163.com>
2017-01-02 15:10:13 +0800

    Update routes.js
    增加收货地址等页面路由

--- js/routes.js
+++ js/routes.js
@@ -4,6 +4,9 @@ import { Route } from 'react-router'
 import app from './containers/app'// 本地服务
 import salesgoods from './containers/salesgoods'// 一元夺宝
 import GoodsInfo from './containers/GoodsInfo'// 详情页
+import address from './containers/address'// 收货地址
+import addressAdd from './containers/addressAdd'// 新增收货地址
+import addressModify from './containers/addressModify'// 修改收货地址
 
 
 const routes = (
@@ -11,6 +14,9 @@ const routes = (
 	    <Route path="/" component={app} />
 	    <Route path="/salesgoods" component={salesgoods} />
 	    <Route path="/GoodsInfo" component={GoodsInfo} />
+	    <Route path="/address" component={address} />
+	    <Route path="/addressAdd" component={addressAdd} />
+	    <Route path="/addressModify" component={addressModify} />
 	</Route>
 
 	)
